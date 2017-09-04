a26c42126834c428e9d6d448a5a9a56c8225a990
benjaminsunderland <ben@sunderland.eu.com>
2017-09-04 08:12:51 -0700

    Questions 1-9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,30 @@
 class OysterCard
 
-  attr_accessor :balance
+  attr_accessor :balance, :in_journey
 
-  def initialize(balance)
-    @balance = balance
+  MAXIMUM = 100
+
+  def initialize
+    @balance = 0
   end
+
+  def top_up(amount)
+    fail "#{MAXIMUM} is over your allowed top-up limit" if (@balance + amount) >= MAXIMUM
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+   fail "You have an insufficient balance" if @balance < 1
+   @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,39 @@
 require 'oystercard'
 
 describe OysterCard do
-  it 'reads the balance to be 0' do
-  oystercard = OysterCard.new(0)
-  expect(oystercard.balance).to eq(0)
+ it 'reads the balance to be 0' do
+  expect(subject.balance).to eq(0)
+ end
+
+ it 'can top up the oystercard' do
+  subject.top_up(5)
+  expect(subject.balance).to eq(5)
+ end
+
+ it 'cannot exceed the top_up limit' do
+  expect{subject.top_up(100)}.to raise_error("#{OysterCard::MAXIMUM} is over your allowed top-up limit")
+ end
+
+it 'should deduct money from the balance' do
+  subject.top_up(10)
+  subject.deduct(5)
+  expect(subject.balance).to eq(5)
+ end
+
+it 'changes to journey state to true when touched in' do
+  subject.top_up(10)
+  subject.touch_in
+  expect(subject.in_journey).to be true
+end
+
+it 'changes the journey state to false when touched out' do
+  subject.top_up(10)
+  subject.touch_in
+  subject.touch_out
+  expect(subject.in_journey).to be false
+ end
+
+it 'will throw an error with an insufficient balance' do
+  expect{subject.touch_in}.to raise_error("You have an insufficient balance")
  end
 end
