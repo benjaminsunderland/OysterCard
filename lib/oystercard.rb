e373c8b2fe629bd54387d6634e4712ab27e1e72a
benjaminsunderland <ben@sunderland.eu.com>
2017-09-04 06:48:01 -0700

    Added a test for balance, designed a method to test

---
+++ lib/oystercard.rb
@@ -0,0 +1,9 @@
+class OysterCard
+
+  attr_accessor :balance
+
+  def initialize(balance)
+    @balance = balance
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,8 @@
-describe Oystercard do
+require 'oystercard'
+
+describe OysterCard do
+  it 'reads the balance to be 0' do
+  oystercard = OysterCard.new(0)
+  expect(oystercard.balance).to eq(0)
+ end
 end
